ee5196480ed9f7f6705fd2ee6825e6b32fe04afd
soonmineom <skdlfma123@gmail.com>
2022-10-24 13:31:00 +0900

    JdbcContext 클래스로 분리하기

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -11,40 +11,16 @@ import java.util.Map;
 public class UserDao {
 
     private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
 
     public UserDao(DataSource dataSource) {
+
         this.dataSource = dataSource;
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy ss) {
-
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = dataSource.getConnection();
-            ps = ss.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
+        this.jdbcContext = new JdbcContext(dataSource);
     }
 
     public void add(final User user) {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
 
@@ -89,7 +65,7 @@ public class UserDao {
     public void deleteAll() {
 
         //jdbcContextWithStatementStrategy(new DeleteAllStrategy());
-        jdbcContextWithStatementStrategy((new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy((new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 return c.prepareStatement("DELETE FROM users");
